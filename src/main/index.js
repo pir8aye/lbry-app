9595e6d7e7a287981d8c3d03ef1d90c6c1df842f
Igor Gassmann <igor@lbry.io>
2018-01-17 15:17:47 -0300

    12

--- src/main/index.js
+++ src/main/index.js
@@ -174,7 +174,7 @@ const isSecondInstance = app.makeSingleInstance(argv => {
   // argv: An array of the second instance’s (command line / deep linked) arguments
 
   let URI;
-  if (process.platform === 'win32') {
+  if (process.platform === 'win32' && String(process.argv[1]).startsWith('lbry')) {
     // Keep only command line / deep linked arguments
     URI = argv[1].replace(/\/$/, '').replace('/#', '#');
   }
